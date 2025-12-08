471a16249fb7cd1e20af8273089ac4f18cad14c6
Louis Alamo <louisgerardoserranoalamo@gmail.com>
2025-12-08 16:20:55 -0600

    feat: Add @NotNull annotation to API properties

--- backend/src/main/java/com/weatherapp/weather_backend/config/WeatherProperties.java
+++ backend/src/main/java/com/weatherapp/weather_backend/config/WeatherProperties.java
@@ -3,6 +3,7 @@ package com.weatherapp.weather_backend.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,8 @@ import lombok.Setter;
 @Setter
 @Getter
 public class WeatherProperties {
-
+    @NotNull
     private String key;
+    @NotNull
     private String url;
 }
