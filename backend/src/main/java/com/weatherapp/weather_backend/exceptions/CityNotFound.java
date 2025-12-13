3aeb31019d24f58679ced49d2ab51bfefa26fbbb
Louis Alamo <louisgerardoserranoalamo@gmail.com>
2025-12-13 15:44:40 -0600

    feat: add serialVersionUID to CityNotFound exception for serialization compatibility

--- backend/src/main/java/com/weatherapp/weather_backend/exceptions/CityNotFound.java
+++ backend/src/main/java/com/weatherapp/weather_backend/exceptions/CityNotFound.java
@@ -1,6 +1,9 @@
 package com.weatherapp.weather_backend.exceptions;
 
 public class CityNotFound extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
     public CityNotFound(String message) {
         super(message);
     }
