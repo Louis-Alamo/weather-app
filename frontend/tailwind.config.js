d5db805639aa5b290bfc41b8616c0b95dd90f997
Louis Alamo <louisgerardoserranoalamo@gmail.com>
2025-12-13 15:49:51 -0600

    feat: add Tailwind CSS configuration for styling

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}"], // 👈 Esto le dice: "Busca clases en mis HTML y JS"
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
