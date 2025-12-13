5ce0b1572a6ba7c7f5001b3189389669ca80aa6f
Louis Alamo <louisgerardoserranoalamo@gmail.com>
2025-12-13 15:43:30 -0600

    refactor: replace System.out.println with logger for application startup message

--- backend/src/main/java/com/weatherapp/weather_backend/WeatherBackendApplication.java
+++ backend/src/main/java/com/weatherapp/weather_backend/WeatherBackendApplication.java
@@ -1,17 +1,20 @@
 package com.weatherapp.weather_backend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication
 @EnableCaching
 public class WeatherBackendApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(WeatherBackendApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherBackendApplication.class, args);
-		System.out.println("Weather Backend Application is running");
+		logger.info("Weather Backend Application is running");
 	}
 
 }
